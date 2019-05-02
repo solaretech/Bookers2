50e0135531f589fdbd76a6005670e9214f9b92c7
solaretech <solaretech.kk@gmail.com>
2019-05-02 10:14:01 +0000

    [Created]Refile機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7f95a17fe72750a791002e976c6c492c0d22abbddb56a729e81b74eaac949e990df13fcfe27e7ebadb0084791da81596b5c8369eba6e932937980bd13dab5d40'
