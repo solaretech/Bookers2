0b7c7295d4d4f0490aa16df02f7467d1f2aae3a7
solaretech <solaretech.kk@gmail.com>
2019-05-02 10:15:46 +0000

    [Updated]Userプロフィール更新機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,8 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   private
