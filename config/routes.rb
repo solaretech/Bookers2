80707f4fc177fbcb49357adf304db9e80fc5178f
solaretech <solaretech.kk@gmail.com>
2019-05-02 08:45:26 +0000

    [Updated]routingにabout追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'books#top'
+  resources :books
+  resources :users, only:[:index, :show, :edit, :update]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books
-  root 'books#top'
+
+  get '/about' => 'books#about', as: 'about'
 end
