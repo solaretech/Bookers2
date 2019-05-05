f75a879718eebea987707b39163af760fc5d45c4
solaretech <solaretech.kk@gmail.com>
2019-05-05 10:20:29 +0000

    [Fix]ルーティング記述順序の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root 'books#top'
-  resources :books
-  resources :users, only:[:index, :show, :edit, :update]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :books
+  resources :users, only:[:index, :show, :edit, :update]
   get '/about' => 'books#about', as: 'about'
 end
